2b831ad35df572200ac61dce6024d24748e765bb
jun-shiqi <3068956090@qq.com>
2025-10-23 17:45:02 +0800

    1

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import numpy as np
+import json
+import os
+import asyncio
+
+
+
+PLUGIN_DIR=os.path.dirname(__file__)
+DATA_DIR=os.path.join(PLUGIN_DIR,"data","help_dic.json")
+
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
@@ -11,14 +21,65 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
     
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.permission_type(filter.PermissionType.ADMIN)
+    @filter.command("添加帮助")
+    async def add_help(self,event,hk:str,hv:str):
+        data_dic={}
+        with open(f"{DATA_DIR}","r",encoding="utf-8") as f:
+            data_dic=json.load(f)
+        
+        if hk in data_dic:
+            yield event.plain_result(f"已有该条目，无法重复创建")
+        else:
+            data_dic[hk]="hv"
+            with open(f"{DATA_DIR}","w",encoding="utf-8") as f:
+                data_j=json.dumps(data_dic,ensure_ascii=False)
+                f.write(data_j)
+            yield event.plain_result(f"恭喜成功创建帮助条目")
+    
+
+    
+    @filter.permission_type(filter.PermissionType.ADMIN)
+    @filter.command("删除帮助")
+    async def del_player(self,event,hk:str):
+        data_dic={}
+        with open(f"{DATA_DIR}","r",encoding="utf-8") as f:
+            data_dic=json.load(f)
+        
+        if hk in data_dic:
+            del data_dic[hk]
+            with open(f"{DATA_DIR}","w",encoding="utf-8") as f:
+                data_j=json.dumps(data_dic,ensure_ascii=False)
+                f.write(data_j)
+            yield event.plain_result(f"恭喜成功删除该条目")
+        else:
+            yield event.plain_result(f"该条目不存在")
+
+
+    @filter.permission_type(filter.PermissionType.ADMIN)
+    @filter.command("修改帮助")
+    async def del_player(self,event,hk:str,nhv:str):
+        data_dic={}
+        with open(f"{DATA_DIR}","r",encoding="utf-8") as f:
+            data_dic=json.load(f)
+        
+        if hk in data_dic:
+            data_dic[hk]=nhv
+            with open(f"{DATA_DIR}","w",encoding="utf-8") as f:
+                data_j=json.dumps(data_dic,ensure_ascii=False)
+                f.write(data_j)
+            yield event.plain_result(f"恭喜成功修改该条目")
+        else:
+            yield event.plain_result(f"该条目不存在")
+    
+
+    @filter.command("帮助")
+    async def help_list(self,event:AstrMessageEvent):
+        data_dic={}
+        with open(f"{DATA_DIR}","r",encoding="utf-8") as f:
+            data_dic=json.load(f)
+        yield event.plain_result("\n".join(f"{key}:{vel}"for key,vel in data_dic.items()) )
+    
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
